59832d49aa279340abb045f46783d31b8613318a
jay27v <jamiexd22@gmail.com>
2024-04-22 20:36:08 +0100

    altered = true

---
+++ src/app/movies/page.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Link from "next/link";
+function page() {
+  return (
+    <div>
+      <nav>
+        <Link className="text-4xl flex justify-left" href={"/"}>
+          Home
+        </Link>
+      </nav>
+      <h3 className="text-7xl flex justify-center">Imaginarium</h3>
+      <h2 className="text-4xl flex justify-center">Movies</h2>
+      <div className="text-3xl flex justify-center">
+        The movies you know and love right at your fingertips
+      </div>
+      <nav className="flex justify-center space-x-4 text-4xl">
+        <button>Sci fi</button>
+        <button>Fantasy</button>
+        <button>Horror</button>
+        <button>Documentary</button>
+        <button>Animation</button>
+        <button>Thriller</button>
+        <button>Comedy</button>
+      </nav>
+    </div>
+  );
+}
+
+export default page;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,39 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-<div>
-<Link href={"/page2"}>go to page 2</Link>;
-<ul>
-    <li>HomePage</li>
-</ul>
-</div>
-);;
+    <div>
+      <h3 className="text-7xl flex justify-center">Imaginarium</h3>
+      <nav className="flex justify-center space-x-4 text-4xl">
+        <Link href={"/movies"}>Movies</Link>
+        <Link href={"/tv"}>Tv shows</Link>
+      </nav>
+      <VideoComponent />
+    </div>
+  );
+}
+export function VideoComponent() {
+  return (
+    <div className="flex justify-center">
+      <div style={{ display: "flex" }}>
+        <div style={{ marginRight: "20px" }}>
+          <video width="600" height="400" controls>
+            <source src={"/movie.mp4"} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        </div>
+        <div style={{ marginRight: "20px" }}>
+          <video width="600" height="400" controls>
+            <source src={"/movie2.mp4"} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        </div>
+        <div>
+          <video width="600" height="400" controls>
+            <source src={"/movie3.mp4"} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        </div>
+      </div>
+    </div>
+  );
 }
